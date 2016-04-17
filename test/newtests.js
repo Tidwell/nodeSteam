7c8023048e933707db29d49b691fea809808147e
Matthew Taylor <mstaveleytaylor@gmail.com>
2016-04-17 21:30:20 +0100

    tests have some bugs still fixing

--- test/newtests.js
+++ test/newtests.js
@@ -152,11 +152,12 @@ describe("Method object validation", function() {
     it("bad steamids should not throw", function() {
       assert.doesNotThrow(function() {
         s.getPlayerItems({
-          gameid: 440,
+          appid: 440,
+          steamid: '76123131232313123112',
           callback: function(err,data) {
-            //if (err) throw new Error(err);
-          },
-          steamid: '765611979ffffff60435530'
+            console.log(err);
+            if (err) throw new Error(err);
+          }
         });
       });
     });
@@ -228,6 +229,35 @@ describe("Request handling", function() {
     });
   });
 
+  it("querying the wrong URL should warn of 404", function(done) {
+    var s = new Steam({
+      apiKey: APIKEY,
+      format: "json"
+    });
+    var obj = {
+      callback: function(err, data) {
+        assert.strictEqual(err, "404 Error was returned from steam API");
+        done();
+      },
+      path: "/some/bad/path",
+    };
+    Steam.prototype.makeRequest(obj);
+  });
+
+  it("querying with an invalid API key should warn of 403", function(done) {
+    var s = new Steam({
+      apiKey: "somebadkey",
+      format: "json"
+    });
+    s.getPlayerSummaries({
+      steamids: ['76561198037414410', '76561197960435530'],
+      callback: function(err,data) {
+        if(err) console.log(err);
+        assert.strictEqual(err, "403 Error: Check your API key is correct");
+        done();
+      }
+    });
+  });
 });
 
 describe("Successful Data Return", function() {
@@ -294,6 +324,114 @@ describe("Successful Data Return", function() {
     s.getPlayerItems({
       appid: 440,
       steamid: '76561197993520601',
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetAssetPrices", function(done) {
+    s.getAssetPrices({
+      appid: 440,
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetAssetClassInfo(1)", function(done) {
+    s.getAssetClassInfo({
+      gameid: 440,
+      classid0: '16891096',
+      class_count: 1,
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetAssetClassInfo(2)", function(done) {
+    s.getAssetClassInfo({
+      appid: 440,
+      classIds: ['16891096',156],
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetPlayerAchievements", function(done) {
+    s.getPlayerAchievements({
+      gameid: 440,
+      steamid: '76561197960435530',
+      l: 'en',
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetRecentlyPlayedGames", function(done) {
+    s.getRecentlyPlayedGames({
+      steamid: '76561197960435530',
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetUserStatsForGame", function(done) {
+    s.getUserStatsForGame({
+      steamid: '76561198120639625',
+      appid: 730,
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetGlobalStatsForGame", function(done) {
+    s.getGlobalStatsForGame({
+      appid: 17740,
+      name: "global.map.emp_isle",
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("IsPlayingSharedGame", function(done) {
+    s.isPlayingSharedGame({
+      steamid: '76561198120639625',
+      appid_playing: 730,
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetSchemaForGame", function(done) {
+    s.getSchemaForGame({
+      appid: 730,
+      callback: function(err, data) { check(done, function() {
+        if (err) return err;
+        assert.isObject(data, "Data is not an object");
+      }); }
+    });
+  });
+
+  it("GetPlayerBans", function(done) {
+    s.getPlayerBans({
+      steamids: '76561198120639625',
       callback: function(err, data) { check(done, function() {
         if (err) return err;
         assert.isObject(data, "Data is not an object");
